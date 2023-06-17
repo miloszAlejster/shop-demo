104b7e391bf0114be957fc9c8034d4d74bdc3e90
szymongasiewski <szymongasiewski999@gmail.com>
2023-06-18 00:02:39 +0200

    More UserService tests. Updated some methods in UserServiceImpl

--- src/test/java/com/pb/service/UserServiceTest.java
+++ src/test/java/com/pb/service/UserServiceTest.java
@@ -85,4 +85,213 @@ public class UserServiceTest {
 
         assertAll(() -> userService.deleteUser(userId));
     }
+
+    @Test
+    public void UserService_CheckEmail_ReturnsTrue() {
+        String email = "existing@example.com";
+
+        when(userRepository.existsByEmail(email)).thenReturn(true);
+
+        boolean found = userService.checkEmail(email);
+
+        Assertions.assertThat(found).isTrue();
+    }
+
+    @Test
+    public void UserService_CheckEmail_ReturnsFalse() {
+        String email = "nonexisting@example.com";
+
+        when(userRepository.existsByEmail(email)).thenReturn(false);
+
+        boolean found = userService.checkEmail(email);
+
+        Assertions.assertThat(found).isFalse();
+    }
+
+    @Test
+    public void UserService_UpdateUserEmail_UserExists_EmailUpdated() {
+        Long userId = 1L;
+        String newEmail = "newemail@example.com";
+
+        User existingUser = new User();
+        existingUser.setId(userId);
+        existingUser.setEmail("oldemail@example.com");
+
+        User updatedUser = new User();
+        updatedUser.setId(userId);
+        updatedUser.setEmail(newEmail);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+
+        userService.updateUserEmail(userId, newEmail);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(updatedUser);
+        Assertions.assertThat(newEmail).isEqualTo(existingUser.getEmail());
+    }
+
+    @Test
+    public void UserService_UpdateUserEmail_UserDontExists_NothingUpdated() {
+        Long userId = 1L;
+        String newEmail = "newemail@example.com";
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.updateUserEmail(userId, newEmail);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    public void UserService_UpdateUserPassword_UserExists_PasswordUpdated() {
+        Long userId = 1L;
+        String newPassword = "newpassword";
+
+        User existingUser = new User();
+        existingUser.setId(userId);
+        existingUser.setPassword("oldpassword");
+
+        User updatedUser = new User();
+        updatedUser.setId(userId);
+        updatedUser.setPassword(newPassword);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+
+        userService.updateUserPassword(userId, newPassword);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(updatedUser);
+        Assertions.assertThat(newPassword).isEqualTo(existingUser.getPassword());
+    }
+
+    @Test
+    public void UserService_UpdateUserPassword_UserDontExists_NothingUpdated() {
+        Long userId = 1L;
+        String newPassword = "newpassword";
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.updateUserPassword(userId, newPassword);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    public void UserService_UpdateUserFirstname_UserExists_FirstnameUpdated() {
+        Long userId = 1L;
+        String newFirstname = "newfistname";
+
+        User existingUser = new User();
+        existingUser.setId(userId);
+        existingUser.setFirstname("oldfirstname");
+
+        User updatedUser = new User();
+        updatedUser.setId(userId);
+        updatedUser.setFirstname(newFirstname);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+
+        userService.updateUserFirstName(userId, newFirstname);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(updatedUser);
+        Assertions.assertThat(newFirstname).isEqualTo(existingUser.getFirstname());
+    }
+
+    @Test
+    public void UserService_UpdateUserFirstname_UserDontExists_NothingUpdated() {
+        Long userId = 1L;
+        String newFirstname = "newfistname";
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.updateUserFirstName(userId, newFirstname);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    public void UserService_UpdateUserLastname_UserExists_LastnameUpdated() {
+        Long userId = 1L;
+        String newLastname = "newlastname";
+
+        User existingUser = new User();
+        existingUser.setId(userId);
+        existingUser.setLastname("oldlastname");
+
+        User updatedUser = new User();
+        updatedUser.setId(userId);
+        updatedUser.setLastname(newLastname);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+
+        userService.updateUserLastName(userId, newLastname);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(updatedUser);
+        Assertions.assertThat(newLastname).isEqualTo(existingUser.getLastname());
+    }
+
+    @Test
+    public void UserService_UpdateUserLastname_UserDontExists_NothingUpdated() {
+        Long userId = 1L;
+        String newLastname = "newlastname";
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.updateUserLastName(userId, newLastname);
+
+        verify(userRepository).findById(userId);
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    public void UserService_UpdateUser_UserExists_UserUpdated() {
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .build();
+
+        User existngUser = User.builder()
+                .id(1L)
+                .firstname("Bob")
+                .lastname("Smith")
+                .email("john.smith@example.com")
+                .build();
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existngUser));
+
+        userService.updateUser(userDto);
+
+        verify(userRepository).save(existngUser);
+
+        Assertions.assertThat(userDto.getFirstname()).isEqualTo(existngUser.getFirstname());
+        Assertions.assertThat(userDto.getLastname()).isEqualTo(existngUser.getLastname());
+        Assertions.assertThat(userDto.getEmail()).isEqualTo(existngUser.getEmail());
+    }
+
+    @Test
+    public void UserService_UpdateUser_UserDontExists_NothingUpdated() {
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .build();
+
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        userService.updateUser(userDto);
+
+        verify(userRepository, never()).save(any(User.class));
+    }
 }
